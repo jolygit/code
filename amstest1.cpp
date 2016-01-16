2012c3bedf76c0bbcbde49a09199f78042471de8
Alex Jaoshvili <alex.jaoshvili@gmail.com>
2016-01-15 20:48:44 -0500

    amtest

--- amstest1.cpp
+++ amstest1.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <typeinfo>
-#include "my.h"
+#include <vector>
+#include <iostream>
+#include <string>
+//#include "my.h"
 struct base;
 struct base_vtable
 
@@ -63,21 +66,52 @@ namespace {
     int kk=0;
   }
 } 
-int main(void)
-{
-  Alex a;
-  //rintf("%d",alex);
-    /* OK!  We're done with our declarations, now we can finally do some
-       polymorphism in C */
-    struct derived1 d;
-    derived1_init(&d,4);
-    struct base *d_ptr = (struct base *)&d;
-    d_ptr->vtable->dance(d_ptr);
 
-    struct base b;
-    base_init(&b);
-    struct base *b_ptr = &b;
-    b_ptr->vtable->dance(b_ptr);
-    return 0;
+class base1{
+public: 
+  int k;
+  base1():k(0){printf("base1 const\n");}
+  ~base1(){
+    printf("base1 destructor\n");
+  }
+};
+class derivedd: public base1{
+  //derivedd(const derivedd&);
+public:
+  static const int d=5;
+  enum {c1=4};
+  derivedd(){printf("derived1 const %d\n",c1);}
+ ~derivedd(){
+    printf("derivedd destructor\n");
+  }
+};
+
+int main()
+{
+  {
+  derivedd d;
+  derivedd d1=d;
+  derivedd d2=d;
+  }
+  return 0;
 }
+
+
+// int main(void)
+// {
+//   Alex a;
+//   //rintf("%d",alex);
+//     /* OK!  We're done with our declarations, now we can finally do some
+//        polymorphism in C */
+//     struct derived1 d;
+//     derived1_init(&d,4);
+//     struct base *d_ptr = (struct base *)&d;
+//     d_ptr->vtable->dance(d_ptr);
+
+//     struct base b;
+//     base_init(&b);
+//     struct base *b_ptr = &b;
+//     b_ptr->vtable->dance(b_ptr);
+//     return 0;
+// }
 

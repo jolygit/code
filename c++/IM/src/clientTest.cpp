80b58f30495a0d7e0ecc7fe0a322f5216161b3c0
Alex Joly <alex.joly@gmail.com>
2016-11-03 14:29:10 -0400

    testing poll on ubuntu

---
+++ c++/IM/src/clientTest.cpp
@@ -0,0 +1,41 @@
+#include <sys/ioctl.h>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int
+main(int argc, char **argv)
+{
+  int			sockfd;
+  int const  myOPEN_MAX=100,MAXLINE=20;
+  struct pollfd	  client[myOPEN_MAX];
+  int maxi,nready,n;
+  char buf[1000];
+  client[0].fd = fileno(stdin);
+  client[0].events = POLLRDNORM;
+  for (int i = 1; i < myOPEN_MAX; i++){
+    client[i].fd = -1;		/* -1 indicates available entry */
+  }
+  maxi = 1;					/* max index into client[] array */
+  printf("enter something\n");
+  for ( ; ; ) {
+    nready = poll(client, maxi+1, -1);
+    for (int i = 0; i <= maxi; i++) {	/* check all clients for data */
+      if ( (sockfd = client[i].fd) < 0)
+	continue;
+      if (client[i].revents & (POLLRDNORM | POLLERR)) {
+	if ( (n = read(sockfd, buf, MAXLINE)) < 0) {
+	  if (i == 0) { //stdin
+	  }
+	  if (--nready <= 0)
+	    break;				/* no more readable descriptors */
+	}
+      }
+    }
+  }
+  return 0;
+}
